d7465ec90d4129fa65c075260d46cb7386dba882
Mootfrost <hello@mootfrost.dev>
2025-07-26 07:25:33 +0000

    Add security.nix

--- security.nix
+++ security.nix
@@ -4,32 +4,13 @@
   lib,
   ...
 }: {
-  services.pcscd.enable = true;
-
-  # secure boot
-  imports = [
-    inputs.lanzaboote.nixosModules.lanzaboote
-  ];
   environment.systemPackages = with pkgs; [
-    sbctl
     tpm2-tools
   ];
-  # Lanzaboote replaces the systemd-boot module.
-  boot.loader.systemd-boot.enable = lib.mkForce false;
-  boot.lanzaboote = {
-    enable = true;
-    pkiBundle = "/etc/secureboot";
-  };
 
-  # tpm
+  
   boot.initrd.systemd.enable = true;
-  boot.initrd.luks.devices.root.crypttabExtraOpts = [
+  boot.initrd.luks.devices.enc.crypttabExtraOpts = [
     "tpm2-device=auto"
-  ];
-
-  # hardening
-  systemd.enableEmergencyMode = false;
-  boot.kernelParams = [
-    "rd.systemd.gpt_auto=0"
   ];
 }
